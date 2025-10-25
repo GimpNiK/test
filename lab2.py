53a59c1f0041063f9fff3ed207317ae34e119cf9
ItisInow <reshfizika@mail.ru>
2025-10-25 12:03:19 +0300

    Add calculator in lab2

--- lab2.py
+++ lab2.py
@@ -7,3 +7,20 @@ def gcd(a:int, b:int)->int:
         a, b = b, a % b
     return a
 print(gcd(6,9))
+
+# 6. Калькулятор
+def calculator(a: float, b: float, operation: str) -> float:
+    match operation:
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "*":
+            return a * b
+        case "/":
+            if b == 0:
+                raise ZeroDivisionError("Ошибка: деление на ноль")
+            return a / b
+        case _:
+            raise TypeError("Ошибка: неподдерживаемая операция. Используйте '+', '-', '*', '/'")
+print(calculator(2,3,"/"))
