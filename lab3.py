89bf7c1e53e52d7289b91eb4d116b10a109d92eb
ItisInow <reshfizika@mail.ru>
2025-11-13 16:38:05 +0300

    Add 6 task

--- lab3.py
+++ lab3.py
@@ -1,9 +1,11 @@
 # 4. Класс «Фигура» (площадь, периметр).
+# 9. Использовать abc для абстрактного класса.
 # 6. Класс со статическим методом.
+
 # 10. Создать класс-итератор.
 
 # 5. Класс для сериализации объектов в JSON.
-# 9. Использовать abc для абстрактного класса.
+
 
 import abc
 class Figure(abc.ABC):
@@ -16,7 +18,7 @@ class Figure(abc.ABC):
 
 class Circle(Figure):
 
-    def __init__(self, radius) -> None:
+    def __init__(self, radius: float) -> None:
         self.radius = radius
     
     def square(self) -> float:
@@ -25,3 +27,7 @@ class Circle(Figure):
     def perimeter(self) -> float:
         import math
         return math.pi * self.radius * 2
+    
+    @staticmethod
+    def is_valid_radius(radius: float):
+        return radius >= 0
