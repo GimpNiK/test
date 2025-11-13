73a4be639066de9f4983a3b43f4e749ac3deba34
ItisInow <reshfizika@mail.ru>
2025-11-13 16:33:23 +0300

    Add 9 task

--- lab3.py
+++ lab3.py
@@ -5,9 +5,23 @@
 # 5. Класс для сериализации объектов в JSON.
 # 9. Использовать abc для абстрактного класса.
 
-class Figure:
+import abc
+class Figure(abc.ABC):
+    @abc.abstractmethod
     def square(self) -> float:
         ...
+    @abc.abstractmethod
     def perimeter(self) -> float:
         ...
+
+class Circle(Figure):
+
+    def __init__(self, radius) -> None:
+        self.radius = radius
     
+    def square(self) -> float:
+        import math
+        return math.pi * self.radius ** 2
+    def perimeter(self) -> float:
+        import math
+        return math.pi * self.radius * 2
