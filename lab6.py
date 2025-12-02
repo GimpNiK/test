affbdf2ed47db847a11e39a16a8f44da4d4d4c8f
ItisInow <reshfizika@mail.ru>
2025-12-02 08:50:55 +0300

    Add route table

--- lab6.py
+++ lab6.py
@@ -6,9 +6,15 @@
 
 from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = ".")
 
 @app.route("/hello")
 def hello():
     return "Hello"
 
+from random import randint
+@app.route("/table")
+def table():
+    return render_template("table.html", header = "Table", table = [[randint(1,10) for column in range(10)] for string in range(3)])
+
+app.run(debug=True)
