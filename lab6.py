50939aea63f7518b66697932d93b5afa992fa0f3
ItisInow <reshfizika@mail.ru>
2025-12-02 08:25:24 +0300

    Add route hello

--- lab6.py
+++ lab6.py
@@ -3,3 +3,12 @@
 # 10. Простейший REST API список задач.
 # 5. REST API для заметок.
 # 9. Подключение SQLAlchemy.
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return "Hello"
+
