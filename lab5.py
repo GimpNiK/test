0ddd1f9f74fb8a69f5c249e7c6dcd922515b7b5c
ItisInow <reshfizika@mail.ru>
2025-12-13 02:40:40 +0300

    Add page 1 for ComboBox in lab5

--- lab5.py
+++ lab5.py
@@ -1,5 +1,34 @@
-# 4. Список (Listbox) и выбор элемента.
-# 6. Прогрессбар.
-# 10. Простейший GUI на PyQt5.
-# 5. Программа таймер/секундомер.
-# 9. Приложение PyQt с таблицей.
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow,QTableWidgetItem, QListWidget, QLabel, QPushButton, QProgressBar, QStackedWidget,QWidget,QVBoxLayout,QComboBox,QTableWidget
+from PyQt6.QtCore import QTimer
+
+app = QApplication(sys.argv)
+
+mainwindow = QMainWindow()
+mainwindow.setWindowTitle("Lab 5")
+
+
+pages = QStackedWidget()
+mainwindow.setCentralWidget(pages)
+
+
+page1 = QWidget()
+page1_layout = QVBoxLayout(page1)
+
+input_os_descr = QLabel("Выберите операционную систему:")
+input_os = QComboBox()
+input_os.addItems(["Windows", "Linux", "MacOS"])
+
+install_btn = QPushButton("Установить")
+
+page1_layout.addWidget(input_os_descr)
+page1_layout.addWidget(input_os)
+page1_layout.addWidget(install_btn)
+
+pages.addWidget(page1)
+
+
+
+mainwindow.adjustSize()
+mainwindow.show()
+app.exec()
