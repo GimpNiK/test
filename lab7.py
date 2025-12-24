3f704f347f6923c6d18c101c3a76524b3d7219e6
ItisInow <reshfizika@mail.ru>
2025-12-24 16:35:19 +0300

    Add creating Item table

--- lab7.py
+++ lab7.py
@@ -3,3 +3,23 @@
 # 10. Использовать SQLAlchemy для одной таблицы.
 # 5. Транзакции.
 # 9. Асинхронный доступ (databases).
+
+
+import asyncio
+from sqlalchemy import Column, Integer, String, select
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    value = Column(Integer)
+
+
+DATABASE_URL = "sqlite+aiosqlite:///test.db"
+engine = create_async_engine(DATABASE_URL, echo=False)
+
