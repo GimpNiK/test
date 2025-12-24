0caa8ace89e4e54b6dd4e0ba4eaa666bf55e0fa9
ItisInow <reshfizika@mail.ru>
2025-12-24 16:36:26 +0300

    Add creating db

--- lab7.py
+++ lab7.py
@@ -23,3 +23,13 @@ class Item(Base):
 DATABASE_URL = "sqlite+aiosqlite:///test.db"
 engine = create_async_engine(DATABASE_URL, echo=False)
 
+async def main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    
+
+    
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
